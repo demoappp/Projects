1a52ca0d99c7307b8d9b78188bed7d5b6a4e6263
YILDIRAY <yildirayildirim016@hotmail.com>
2019-04-29 16:28:50 +0300

    Add project files.

---
+++ WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+   
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        IFirebaseClient client;
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "IOPdADRVrgptFuHeLnaOrCfcKBwgpNtaszu5WfOt",
+            BasePath= "https://aspnet-d6f2e.firebaseio.com/"
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            if(client != null)
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+                 
+            }
+        }
+    }
+}
